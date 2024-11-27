78c47e62568992a4bdcc6abd642774d3a9690e89
Justice Gooch <justicegooch@hotmail.com>
2024-11-27 08:14:14 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  basePath: '/demos/cybersweeper',
+  assetPrefix: '/demos/cybersweeper',
+  trailingSlash: true,
+  images: { unoptimized: true, loader: 'akamai', path: '/demos/cybersweeper' },
 };
 
 module.exports = nextConfig;
